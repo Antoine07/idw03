88867552d572ee48e9f66836cb08cb6b1d024f7c
Antoine07 <antoine.lucsko@gmail.com>
2021-11-22 12:14:28 +0100

    fk

---
+++ MySQL/cours/Exercices/02_foreign_key.sql
@@ -0,0 +1,11 @@
+
+ALTER TABLE pilots 
+ADD CONSTRAINT fk_pilots_compagny_compagnies_comp 
+FOREIGN KEY (compagny) 
+REFERENCES compagnies(`comp`);
+
+-- suppression de la clé étrangère
+ALTER TABLE pilots DROP FOREIGN KEY fk_pilots_compagny_compagnies_comp;
+
+-- suppression de la KEY (nommé)
+ALTER TABLE pilots KEY fk_pilots_compagny_compagnies_comp;
